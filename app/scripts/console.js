9a7287848bdddd5bf484184c6a8b8d0821cce332
Oliver Sanderson <osanderson@wpi.edu>
2018-02-06 03:17:11 -0500

    Working search filter

--- app/scripts/console.js
+++ app/scripts/console.js
@@ -247,3 +247,24 @@ function getMapUrl(){
     return url;
 
 }
+// ------------------------- List Stuff ---------------------- //
+function filterTable() {
+    // Declare variables
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("searchFilter");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("reportDisplay");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
