c8aad674f92496a633c6c15f07119bf78e693c72
Oliver Sanderson <osanderson@wpi.edu>
2018-02-06 02:12:36 -0500

    Added timeout and age options to geolocation

--- app/scripts/console.js
+++ app/scripts/console.js
@@ -214,13 +214,24 @@ function downloadCSV(args) {
 // Gets lat and long with geolocation api, sets map center to current location
 window.onload = function() {
     var startPos;
+    var geoOptions = {
+        timeout: 10 * 1000
+    };
     var geoSuccess = function(position) {
         startPos = position;
         document.getElementById('startLat').innerHTML = startPos.coords.latitude;
         document.getElementById('startLon').innerHTML = startPos.coords.longitude;
         map.setAttribute("src", getMapUrl());
     };
-    navigator.geolocation.getCurrentPosition(geoSuccess);
+    var geoError = function(error) {
+        console.log('Error occurred. Error code: ' + error.code);
+        // error.code can be:
+        //   0: unknown error
+        //   1: permission denied
+        //   2: position unavailable (error response from location provider)
+        //   3: timed out
+    };
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
 };
 
 // Creates url to call Google Maps API
