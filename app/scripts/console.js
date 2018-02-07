8588f3c46ec439d092d24dbba9fde17c0e0b93db
Oliver Sanderson <osanderson@wpi.edu>
2018-02-06 22:14:26 -0500

    Search for any field working

--- app/scripts/console.js
+++ app/scripts/console.js
@@ -250,7 +250,7 @@ function getMapUrl(){
 // ------------------------- List Stuff ---------------------- //
 function filterTable() {
     // Declare variables
-    var input, filter, table, tr, td, i;
+    var input, filter, table, tr, td, i, j;
     input = document.getElementById("searchFilter");
     filter = input.value.toUpperCase();
     table = document.getElementById("reportDisplay");
@@ -258,12 +258,15 @@ function filterTable() {
 
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[1];
-        if (td) {
-            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
+        td = tr[i].getElementsByTagName("td");
+        for (j = 0; j < td.length; j++) {
+            if (td[j]) {
+                if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                    j = td.length; // If row should be shown, stop checking
+                } else {
+                    tr[i].style.display = "none";
+                }
             }
         }
     }
