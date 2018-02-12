2dd2a81db84be8906f9f22c96fb7964f6f0a685e
Oliver Sanderson <osanderson@wpi.edu>
2018-02-11 23:01:52 -0500

    Testing merge

--- app/scripts/input.js
+++ app/scripts/input.js
@@ -1,6 +1,19 @@
 var form = document.querySelector("#form");
 var submitBtn = document.querySelector("#submitBtn");
 
+var config = {
+    apiKey: "AIzaSyCFWzxl0VLYePJ-5O8U5umWWNJLT7TG9Fo",
+    authDomain: "urmatt-app.firebaseapp.com",
+    databaseURL: "https://urmatt-app.firebaseio.com",
+    projectId: "urmatt-app",
+    storageBucket: "urmatt-app.appspot.com",
+    messagingSenderId: "523826665141"
+};
+if(!firebase.apps.length){
+    firebase.initializeApp(config);
+}
+var firestore = firebase.firestore();
+
 function submitReport(data){
     firestore.collection("reports").add(data);
 }
@@ -8,10 +21,11 @@ function submitReport(data){
 form.addEventListener("submit", function(e) {
     e.preventDefault();
     var data = {};
-    for(var i = 0; i < form.elements.length; i++){
+    for(var i = 0; i < 1; i++){
         let key = form.elements[i].name;
         let val = form.elements[i].value;
-        data[key] = val;
+        if (val != null && key != null) data[key] = val;
+
     }
     console.log(data);
     submitReport(data);
