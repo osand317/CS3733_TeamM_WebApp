ca6ae0bce73fcbc264bc2f8330f6a00c0f77eead
aleksanderibro <35206581+aleksanderibro@users.noreply.github.com>
2018-02-22 11:04:30 +0700

    On click marker and saving coordinates implemented

--- app/scripts/map.js
+++ app/scripts/map.js
@@ -1,4 +1,7 @@
 var map;
+var markers = [];
+var marker;
+var coords = [];
 function initMap() {
 
   map = new google.maps.Map(document.getElementById('map'), {
@@ -17,9 +20,22 @@ function initMap() {
             icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
             map: map
           });
+          addMarker(initialLocation);
+
       });
     }
   drawMap();
+  map.addListener('click', function(e) {
+    markers[0].setMap(null);
+    markers=[];
+    clickLocation =  new google.maps.LatLng(e.latLng.lat(), e.latLng.lng());
+    addMarker(clickLocation);
+    coords[0] = e.latLng.lat();
+    coords[1] = e.latLng.lng();
+    // console.log(e.latLng.lng());
+
+  });
+
   // Create a <script> tag and set the USGS URL as the source.
   // var script = document.createElement('script');
   // // This example uses a local copy of the GeoJSON stored at
@@ -27,6 +43,24 @@ function initMap() {
   // script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
   // document.getElementsByTagName('head')[0].appendChild(script);
 }
+
+// Adds a marker to the map and push to the array.
+function addMarker(location) {
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+  markers.push(marker);
+}
+
+function storeLocation() {
+  console.log(markers[0].position);
+  console.log(coords);
+  localStorage.setItem('latitude', coords[0]);
+  localStorage.setItem('longitude', coords[1]);
+  console.log(localStorage);
+  window.location = "farmerAccountCreation.html";
+};
 
 // Loop through the results array and place a marker for each
 // set of coordinates.
