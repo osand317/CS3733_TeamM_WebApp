424f6edd4752398fbddee7bd41d4a83bad129f5e
aleksanderibro <35206581+aleksanderibro@users.noreply.github.com>
2018-02-22 22:58:24 +0700

    Month and week options for check in

--- app/scripts/map.js
+++ app/scripts/map.js
@@ -2,6 +2,7 @@ var map;
 var markers = [];
 var marker;
 var coords = [];
+var counter = 0;
 function initMap() {
 
   map = new google.maps.Map(document.getElementById('map'), {
@@ -20,21 +21,27 @@ function initMap() {
             icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
             map: map
           });
-          addMarker(initialLocation);
+          // addMarker(initialLocation);
 
       });
     }
-  drawMap();
-  map.addListener('click', function(e) {
-    markers[0].setMap(null);
-    markers=[];
-    clickLocation =  new google.maps.LatLng(e.latLng.lat(), e.latLng.lng());
-    addMarker(clickLocation);
-    coords[0] = e.latLng.lat();
-    coords[1] = e.latLng.lng();
+    if (localStorage.getItem('Page') == "CheckIn") {
+      drawCheckins();
+    } else {
+      map.addListener('click', function(e) {
+        markers[0].setMap(null);
+        markers=[];
+        clickLocation =  new google.maps.LatLng(e.latLng.lat(), e.latLng.lng());
+        addMarker(clickLocation);
+        coords[0] = e.latLng.lat();
+        coords[1] = e.latLng.lng();
+    });
+  }
+  drawFarms();
+
     // console.log(e.latLng.lng());
 
-  });
+  };
 
   // Create a <script> tag and set the USGS URL as the source.
   // var script = document.createElement('script');
@@ -42,7 +49,7 @@ function initMap() {
   // // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
   // script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
   // document.getElementsByTagName('head')[0].appendChild(script);
-}
+
 
 // Adds a marker to the map and push to the array.
 function addMarker(location) {
@@ -51,6 +58,13 @@ function addMarker(location) {
     map: map
   });
   markers.push(marker);
+}
+
+function addPoint(location) {
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
 }
 
 function storeLocation() {
@@ -77,15 +91,38 @@ function storeLocation() {
 
 
 
-function drawMap() {
-  var checkInRef = firestore.collection("checkIns");
+function drawCheckins() {
+  var checkInRef = firestore.collection("checkIns").where("uid", '==', localStorage.getItem("userID"));
+  checkInRef.get().then(
+    window.eqfeed_callback = function (doc) {
+      doc.forEach(function (coordinates) {
+        console.log(coordinates.data().timestamp);
+        if (Date.now() - Date.parse(coordinates.data().timestamp) <= 2419200000 && document.getElementById('monthBox').checked) {
+          counter++;
+          console.log(counter);
+          var latLng = new google.maps.LatLng(coordinates.data().latitude, coordinates.data().longitude);
+          addPoint(latLng);
+        } else if(Date.now() - Date.parse(coordinates.data().timestamp) <= 604800000 && document.getElementById('weekBox').checked){
+          counter++;
+          console.log(counter);
+          var latLng = new google.maps.LatLng(coordinates.data().latitude, coordinates.data().longitude);
+          addPoint(latLng);
+        }
+      });
+    }
+  );
+};
+
+function drawFarms() {
+  var checkInRef = firestore.collection("users");
   checkInRef.get().then(
     window.eqfeed_callback = function (doc) {
       doc.forEach(function (coordinates) {
         var latLng = new google.maps.LatLng(coordinates.data().latitude, coordinates.data().longitude);
         var marker = new google.maps.Marker({
           position: latLng,
-          map: map
+          map: map,
+          icon: 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png'
         });
       });
     }
