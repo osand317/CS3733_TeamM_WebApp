324380f417d90c2fe545024f8658342550d2f406
Oliver Sanderson <osanderson@wpi.edu>
2018-02-24 08:36:47 -0500

    Index tweaks

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,12 +1,17 @@
-document.querySelector('#accountBtn').addEventListener('click', function(){
+var accountBtn = document.querySelector('#accountBtn');
+var consoleBtn = document.querySelector('#consoleBtn');
+var formsBtn = document.querySelector('#formsBtn');
+var checkInBtn = document.querySelector('#checkInBtn');
+
+accountBtn.addEventListener('click', function(){
     window.location = '/farmerProfiles.html';
 });
-document.querySelector('#consoleBtn').addEventListener('click', function(){
+consoleBtn.addEventListener('click', function(){
     window.location = '/console.html';
 });
-document.querySelector('#formsBtn').addEventListener('click', function(){
+formsBtn.addEventListener('click', function(){
     window.location = '/fillform.html';
 });
-document.querySelector('#checkInBtn').addEventListener('click', function(){
+checkInBtn.addEventListener('click', function(){
     window.location = '/checkIn.html';
 });
