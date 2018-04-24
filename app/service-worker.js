450cd8daf011b1c77b3e2f1a864fa9ab98b21f8d
Oliver Sanderson <osanderson@wpi.edu>
2018-04-23 22:44:05 -0400

    Deployed successfully

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,44 +1,44 @@
-// importScripts('workbox-sw.prod.v2.1.2.js');
-//
-// const workboxSW = new WorkboxSW();
-// workboxSW.precache([]);
-//
-// workboxSW.router.registerRoute('https://fonts.googleapis.com/(.*)',
+importScripts('workbox-sw.prod.v2.1.2.js');
+
+const workboxSW = new WorkboxSW();
+workboxSW.precache([]);
+
+workboxSW.router.registerRoute('https://fonts.googleapis.com/(.*)',
+  workboxSW.strategies.cacheFirst({
+    cacheName: 'googleapis',
+    cacheExpiration: {
+      maxEntries: 20
+    },
+    cacheableResponse: {statuses: [0, 200]}
+  })
+);
+
+workboxSW.router.registerRoute('https://fonts.gstatic.com/(.*)',
+    workboxSW.strategies.cacheFirst({
+        cacheName: 'gstatic',
+        cacheExpiration: {
+            maxEntries: 20
+        },
+        cacheableResponse: {statuses: [0, 200]}
+    })
+);
+
+workboxSW.router.registerRoute('https://code.getmdl.io/(.*)',
+    workboxSW.strategies.cacheFirst({
+        cacheName: 'mdl',
+        cacheExpiration: {
+            maxEntries: 20
+        },
+        cacheableResponse: {statuses: [0, 200]}
+    })
+);
+
+// // We want no more than 50 images in the cache. We check using a cache first strategy
+// workboxSW.router.registerRoute(/\.(?:png|gif|jpg)$/,
 //   workboxSW.strategies.cacheFirst({
-//     cacheName: 'googleapis',
+//     cacheName: 'images-cache',
 //     cacheExpiration: {
-//       maxEntries: 20
-//     },
-//     cacheableResponse: {statuses: [0, 200]}
+//       maxEntries: 50
+//     }
 //   })
 // );
-//
-// workboxSW.router.registerRoute('https://fonts.gstatic.com/(.*)',
-//     workboxSW.strategies.cacheFirst({
-//         cacheName: 'gstatic',
-//         cacheExpiration: {
-//             maxEntries: 20
-//         },
-//         cacheableResponse: {statuses: [0, 200]}
-//     })
-// );
-//
-// workboxSW.router.registerRoute('https://code.getmdl.io/(.*)',
-//     workboxSW.strategies.cacheFirst({
-//         cacheName: 'mdl',
-//         cacheExpiration: {
-//             maxEntries: 20
-//         },
-//         cacheableResponse: {statuses: [0, 200]}
-//     })
-// );
-//
-// // // We want no more than 50 images in the cache. We check using a cache first strategy
-// // workboxSW.router.registerRoute(/\.(?:png|gif|jpg)$/,
-// //   workboxSW.strategies.cacheFirst({
-// //     cacheName: 'images-cache',
-// //     cacheExpiration: {
-// //       maxEntries: 50
-// //     }
-// //   })
-// // );
